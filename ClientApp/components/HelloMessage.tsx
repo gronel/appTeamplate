91aae2264c3fb2cdc3965af9fbdfba1c238a41ea
Ronel Gonzales <rgonzales@timelessmedical.com>
2018-05-14 13:44:45 +0800

    first commmit

---
+++ ClientApp/components/HelloMessage.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { RouteComponentProps } from 'react-router';
+
+export   class HelloMessage  extends React.Component<RouteComponentProps<{}>, {}>{
+    public render(){
+        return     <div>
+        <p>HI Ronel gonzales</p>
+    </div>
+    }
+   
+}
+
+ 
